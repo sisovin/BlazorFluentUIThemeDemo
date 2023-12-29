b25d27995273f04afbec8f151de52f996db1500b
Sisovin Chieng <sisovin@gmail.com>
2023-12-29 10:53:05 +0700

    Add project files.

---
+++ BlazorFluentUIClassLibrary.Shared/DemoLogger.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlazorFluentUIClassLibrary.Shared
+{
+    public delegate void OnLogHandler(string text);
+    public static class DemoLogger
+    {
+        public static event OnLogHandler? OnLog;
+        public static void WriteLine(string text)
+        {
+            OnLog?.Invoke(text);
+        }
+    }
+}
---
+++ BlazorFluentUIThemeDemo/Components/SiteSettings.razor.cs
@@ -0,0 +1,25 @@
+using BlazorFluentUIClassLibrary.Shared;
+using Microsoft.FluentUI.AspNetCore.Components;
+
+namespace BlazorFluentUIThemeDemo.Components;
+
+public partial class SiteSettings
+{
+    private IDialogReference? _dialog;
+
+    private async Task OpenSiteSettingsAsync()
+    {
+        DemoLogger.WriteLine($"Open site settings");
+        _dialog = await DialogService.ShowPanelAsync<SiteSettingsPanel>(new DialogParameters()
+        {
+            ShowTitle = true,
+            Title = "Site settings",
+            Alignment = HorizontalAlignment.Right,
+            PrimaryAction = "OK",
+            SecondaryAction = null,
+            ShowDismiss = true
+        });
+
+        DialogResult result = await _dialog.Result;
+    }
+}
